9229d10fa66db495629d95acbf4546ebdf05962b
metacube-vaibhav-zambad <vaibhav.zambad@metacube.com>
2016-09-22 20:24:42 +0530

    OAuth 2 POC Added .. Need a magical final touch to work

---
+++ OAuth2.0POC/src/resource/OAuthDetails.java
@@ -0,0 +1,239 @@
+package resource;
+
+public class OAuthDetails {
+	
+	private String scope;
+	private String state;
+	private String grant_type;
+	private String client_id;
+	private String client_secret;
+	private String access_token;
+	private String refresh_token;
+	private String redirect_uri;
+	private String authentication_server_url;
+	private String token_endpoint_url;
+	private String resource_server_url;
+	private String approval_prompt_key;
+	private String approval_prompt_value;
+	private String access_type_key;
+	private String access_type_value;
+	private String username;
+	private String password;
+	private boolean isAccessTokenRequest;
+	/**
+	 * @return the scope
+	 */
+	public String getScope() {
+		return scope;
+	}
+	/**
+	 * @param scope the scope to set
+	 */
+	public void setScope(String scope) {
+		this.scope = scope;
+	}
+	/**
+	 * @return the state
+	 */
+	public String getState() {
+		return state;
+	}
+	/**
+	 * @param state the state to set
+	 */
+	public void setState(String state) {
+		this.state = state;
+	}
+	/**
+	 * @return the grant_type
+	 */
+	public String getGrant_type() {
+		return grant_type;
+	}
+	/**
+	 * @param grant_type the grant_type to set
+	 */
+	public void setGrant_type(String grant_type) {
+		this.grant_type = grant_type;
+	}
+	/**
+	 * @return the client_id
+	 */
+	public String getClient_id() {
+		return client_id;
+	}
+	/**
+	 * @param client_id the client_id to set
+	 */
+	public void setClient_id(String client_id) {
+		this.client_id = client_id;
+	}
+	/**
+	 * @return the client_secret
+	 */
+	public String getClient_secret() {
+		return client_secret;
+	}
+	/**
+	 * @param client_secret the client_secret to set
+	 */
+	public void setClient_secret(String client_secret) {
+		this.client_secret = client_secret;
+	}
+	/**
+	 * @return the access_token
+	 */
+	public String getAccess_token() {
+		return access_token;
+	}
+	/**
+	 * @param access_token the access_token to set
+	 */
+	public void setAccess_token(String access_token) {
+		this.access_token = access_token;
+	}
+	/**
+	 * @return the refresh_token
+	 */
+	public String getRefresh_token() {
+		return refresh_token;
+	}
+	/**
+	 * @param refresh_token the refresh_token to set
+	 */
+	public void setRefresh_token(String refresh_token) {
+		this.refresh_token = refresh_token;
+	}
+	/**
+	 * @return the redirect_uri
+	 */
+	public String getRedirect_uri() {
+		return redirect_uri;
+	}
+	/**
+	 * @param redirect_uri the redirect_uri to set
+	 */
+	public void setRedirect_uri(String redirect_uri) {
+		this.redirect_uri = redirect_uri;
+	}
+	/**
+	 * @return the authentication_server_url
+	 */
+	public String getAuthentication_server_url() {
+		return authentication_server_url;
+	}
+	/**
+	 * @param authentication_server_url the authentication_server_url to set
+	 */
+	public void setAuthentication_server_url(String authentication_server_url) {
+		this.authentication_server_url = authentication_server_url;
+	}
+	/**
+	 * @return the token_endpoint_url
+	 */
+	public String getToken_endpoint_url() {
+		return token_endpoint_url;
+	}
+	/**
+	 * @param token_endpoint_url the token_endpoint_url to set
+	 */
+	public void setToken_endpoint_url(String token_endpoint_url) {
+		this.token_endpoint_url = token_endpoint_url;
+	}
+	/**
+	 * @return the resource_server_url
+	 */
+	public String getResource_server_url() {
+		return resource_server_url;
+	}
+	/**
+	 * @param resource_server_url the resource_server_url to set
+	 */
+	public void setResource_server_url(String resource_server_url) {
+		this.resource_server_url = resource_server_url;
+	}
+	/**
+	 * @return the approval_prompt_key
+	 */
+	public String getApproval_prompt_key() {
+		return approval_prompt_key;
+	}
+	/**
+	 * @param approval_prompt_key the approval_prompt_key to set
+	 */
+	public void setApproval_prompt_key(String approval_prompt_key) {
+		this.approval_prompt_key = approval_prompt_key;
+	}
+	/**
+	 * @return the approval_prompt_value
+	 */
+	public String getApproval_prompt_value() {
+		return approval_prompt_value;
+	}
+	/**
+	 * @param approval_prompt_value the approval_prompt_value to set
+	 */
+	public void setApproval_prompt_value(String approval_prompt_value) {
+		this.approval_prompt_value = approval_prompt_value;
+	}
+	/**
+	 * @return the access_type_key
+	 */
+	public String getAccess_type_key() {
+		return access_type_key;
+	}
+	/**
+	 * @param access_type_key the access_type_key to set
+	 */
+	public void setAccess_type_key(String access_type_key) {
+		this.access_type_key = access_type_key;
+	}
+	/**
+	 * @return the access_type_value
+	 */
+	public String getAccess_type_value() {
+		return access_type_value;
+	}
+	/**
+	 * @param access_type_value the access_type_value to set
+	 */
+	public void setAccess_type_value(String access_type_value) {
+		this.access_type_value = access_type_value;
+	}
+	/**
+	 * @return the username
+	 */
+	public String getUsername() {
+		return username;
+	}
+	/**
+	 * @param username the username to set
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	/**
+	 * @return the isAccessTokenRequest
+	 */
+	public boolean isAccessTokenRequest() {
+		return isAccessTokenRequest;
+	}
+	/**
+	 * @param isAccessTokenRequest the isAccessTokenRequest to set
+	 */
+	public void setAccessTokenRequest(boolean isAccessTokenRequest) {
+		this.isAccessTokenRequest = isAccessTokenRequest;
+	}
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Project_IT_Resource
-This repository contains all the documents and development code related to IT Resource Project based on Ruby On Rails.
