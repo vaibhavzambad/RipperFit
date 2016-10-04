0a7966f877711a46a509e118f6c768afba9804ca
metacube-vaibhav-zambad <vaibhav.zambad@metacube.com>
2016-10-04 21:05:24 +0530

    Base Project Added

---
+++ Resource_Tracker/src/main/java/com/resource_tracker/model/Designation.java
@@ -0,0 +1,72 @@
+package com.resource_tracker.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="designation")
+public class Designation {
+	
+	@Id
+	@Column(name="designation_id")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int designationId;
+	
+	@Column(name="designation_name")
+	private String designationName;
+	
+	@OneToMany(cascade={CascadeType.ALL},fetch=FetchType.LAZY)
+	@JoinColumn(name="parent_designation_id")
+	private Designation designation;
+
+	/**
+	 * @return the designationId
+	 */
+	public int getDesignationId() {
+		return designationId;
+	}
+
+	/**
+	 * @param designationId the designationId to set
+	 */
+	public void setDesignationId(int designationId) {
+		this.designationId = designationId;
+	}
+
+	/**
+	 * @return the designationName
+	 */
+	public String getDesignationName() {
+		return designationName;
+	}
+
+	/**
+	 * @param designationName the designationName to set
+	 */
+	public void setDesignationName(String designationName) {
+		this.designationName = designationName;
+	}
+
+	/**
+	 * @return the parentDesignationId
+	 */
+	public Designation getParentDesignationId() {
+		return designation;
+	}
+
+	/**
+	 * @param parentDesignationId the parentDesignationId to set
+	 */
+	public void setParentDesignationId(Designation designation) {
+		this.designation = designation;
+	} 
+	
+}
---
+++ Resource_Tracker/src/main/java/com/resource_tracker/model/Employee.java
@@ -0,0 +1,209 @@
+package com.resource_tracker.model;
+
+import java.util.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="employee")
+public class Employee {
+	
+	@Id
+	@Column(name="employee_id")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int employeeId;
+	
+	@Column(name="email",unique = true)
+	private String email;
+	
+	@Column(name="password")
+	private String password;
+	
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="last_name")
+	private String lastName;
+	
+	@Column(name="dob")
+	private Date dateOfBirth;
+	
+	@Column(name="gender")
+	private String gender;
+	
+	@Column(name="contact_number")
+	private String contactNumber;
+	
+	@Column(name="address")
+	private String address;
+	
+	@ManyToOne(cascade={CascadeType.ALL},optional=false,fetch=FetchType.LAZY)
+	@JoinColumn(name="designation_id")
+	private Designation designation;
+	
+	@Column(name="profile_picture")
+	private byte[] profilePicture;
+
+	/**
+	 * @return the employeeId
+	 */
+	public int getEmployeeId() {
+		return employeeId;
+	}
+
+	/**
+	 * @param employeeId the employeeId to set
+	 */
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	/**
+	 * @return the dateOfBirth
+	 */
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	/**
+	 * @param dateOfBirth the dateOfBirth to set
+	 */
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	/**
+	 * @return the gender
+	 */
+	public String getGender() {
+		return gender;
+	}
+
+	/**
+	 * @param gender the gender to set
+	 */
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	/**
+	 * @return the contactNumber
+	 */
+	public String getContactNumber() {
+		return contactNumber;
+	}
+
+	/**
+	 * @param contactNumber the contactNumber to set
+	 */
+	public void setContactNumber(String contactNumber) {
+		this.contactNumber = contactNumber;
+	}
+
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return address;
+	}
+
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	/**
+	 * @return the designation
+	 */
+	public Designation getDesignation() {
+		return designation;
+	}
+
+	/**
+	 * @param designation the designation to set
+	 */
+	public void setDesignation(Designation designation) {
+		this.designation = designation;
+	}
+
+	/**
+	 * @return the profilePicture
+	 */
+	public byte[] getProfilePicture() {
+		return profilePicture;
+	}
+
+	/**
+	 * @param profilePicture the profilePicture to set
+	 */
+	public void setProfilePicture(byte[] profilePicture) {
+		this.profilePicture = profilePicture;
+	}
+}
