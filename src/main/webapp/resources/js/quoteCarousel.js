2b49bcbfb1faa29dd3d36b0323d5ba2bf5b81de1
metashobhitagarwal <shobhit.agarwal@metacube.com>
2016-10-20 02:55:32 +0530

    DashBoard HomePage Done!!!

---
+++ src/main/webapp/resources/js/quoteCarousel.js
@@ -0,0 +1,8 @@
+// When the DOM is ready, run this function
+$(document).ready(function() {
+  //Set the carousel options
+  $('#quote-carousel').carousel({
+    pauseOnHover: true,
+    interval: 1700,
+  });
+    });
