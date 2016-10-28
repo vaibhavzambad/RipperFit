9232bb336aebc2e6267e1c556d7e1f6a8c6f504b
metacube-vaibhav-zambad <vaibhav.zambad@metacube.com>
2016-10-28 15:55:25 +0530

    Some suggested changes has been done

--- src/main/webapp/resources/js/controllers/forgotPasswordCtrl.js
+++ src/main/webapp/resources/js/controllers/forgotPasswordCtrl.js
@@ -1,5 +1,7 @@
 app.controller('forgotPasswordCtrl', function($scope, $http, $window, $filter) {
 	$scope.forgotPassword = function(email) {
+		var message = angular.element(document.querySelector('#message'));
+		
 		$http({
 			method: 'PUT',
 			url: "/RipperFit/employee/forgetPassword",
@@ -21,7 +23,7 @@ app.controller('forgotPasswordCtrl', function($scope, $http, $window, $filter) {
 					'Content-Type': 'application/json'
 				}
 			}).then(function(response) {
-				$window.location.href = 'signIn.html';
+				message.text("Please check your email account .A password has been sent.");
 			});
 		});
 	}
